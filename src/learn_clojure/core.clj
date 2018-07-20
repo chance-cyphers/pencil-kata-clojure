808ab9aa9190f470bd270db5e1fed84b11d6b77a
chance <chance.a.cyphers@gmail.com>
2018-07-20 13:14:48 -0400

    Check command before writing

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -1,13 +1,23 @@
-(ns learn-clojure.core)
+(ns learn-clojure.core
+  (:require [clojure.string :as str]))
 
 (defn getInput []
-  (println "What do you want to write?")
+  (println "Options:\n\twrite [text]")
   (read-line))
 
-(defn doCommand [oldText]
-  (let [newText (str oldText (getInput))]
-    (println (str "text:\n" newText))
-    (doCommand newText)))
+(defn write [oldText input]
+  (let [newText (str oldText input)]
+    (println (str "\nPaper:\n" newText "\n"))
+    newText))
+
+(defn processCommand [text]
+  (let [input (getInput) command (first (str/split input #" "))]
+    (if (= command "write")
+      (let [options (subs input (+ (count command) 1))]
+        (processCommand (write text options)))
+      (
+        (println "Not a valid command")
+        (processCommand text)))))
 
 (defn -main [& args]
-  (doCommand ""))
+  (processCommand ""))
